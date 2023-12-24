72d5ee96e5d0771315ad0f6720ffebe74b0cf41c
Pikard86 <pikard86@gmail.com>
2023-12-24 14:34:20 +0000

    fix build

--- bin/tools/build_tools.sh
+++ bin/tools/build_tools.sh
@@ -2,26 +2,46 @@ export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
-wget https://ftp.gnu.org/gnu/gcc/gcc-7.5.0/gcc-7.5.0.tar.gz
+echo Downloading binutils
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.gz
+
+echo Extracting binutils archive
 tar -xvf binutils-2.34.tar.gz 
-tar -xfv  gcc-7.5.0.tar.gz 
+
+
 mkdir binutils-build
 cd binutils-build/
+
+echo Building binutils
 ../binutils-2.34/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls
 make
 make install
 cd ..
 
+echo $PWD
+
+
+
+echo Downloading gcc 7.5.0
+wget https://ftp.gnu.org/gnu/gcc/gcc-7.5.0/gcc-7.5.0.tar.gz
+tar -xvf  gcc-7.5.0.tar.gz 
+
 mkdir gcc-build
+
 cd gcc-build/
+
+echo Building custom gcc 
+
 which -- $TARGET-as || echo $TARGET-as is not in the PATH
 ../gcc-7.5.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
-make instal
+make install
 make all-target-libgcc
 make install-gcc
-make install-target-libgccl
+make install-target-libgcc
+cd ..
+
+echo $PWD
 
 rm binutils-2.34.tar.gz
 rm gcc-7.5.0.tar.gz
